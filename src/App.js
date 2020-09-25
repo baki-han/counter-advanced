70885b14b1279b65bbebddd1e3d66027219dd045
baki-han <baki47hanma@gmail.com>
2020-09-25 07:19:56 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import List from "./List";
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
+  const [list, setList] = useState([]);
+
+  //Todo add new counter row
+  const addButton = (valueItem) =>{
+    const newButtons = [];
+    for(let i = 1; i <= Number(valueItem); i++){
+      newButtons.push(i);
+    }
+
+    const obj = {
+      value: Number(valueItem),
+      id: uuidv4(),
+      buttonsArray: newButtons,
+    };
+    const newArray = [...list, obj];
+    setList(newArray);
+  }
+
+  // Todo update counter
+  const counterUpdate =(id, value) =>{
+     const newList = list.map(el=>{
+         if(el.id === id){return {...el, value: el.value + value};}
+         else{return el;}
+     })
+     setList(newList);
+  }
+
+  // Todo delete row
+    const deleteRow = (id) =>{
+      setList(list.filter(el => el.id !== id));
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Header addButton={addButton} />
+        <hr/>
+        <List list={list}
+              counterUpdate={counterUpdate}
+              deleteRow={deleteRow}
+        />
+      </div>
   );
 }
 
